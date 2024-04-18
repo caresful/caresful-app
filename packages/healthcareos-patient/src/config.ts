3b609476b9fc74c589d8e40cb4ff82ef4496874a
klikz-dev <murrell@caresful.com>
2024-04-18 20:11:10 +0500

    feat: setup healthcare-patient package

---
+++ packages/healthcareos-patient/src/config.ts
@@ -0,0 +1,5 @@
+// Default values work on localhost:3000 and foomedical.com
+// Replace these values with your own values for production
+export const MEDPLUM_PROJECT_ID = 'a0736b93-f717-4fa9-868f-9a32cee8d92c';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '687949780985-j6erf2gg0f133rqalukqa3vcf45ltaf2.apps.googleusercontent.com';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL';
